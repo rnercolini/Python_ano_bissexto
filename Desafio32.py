69ed9f70281f3f9b86705f75a3edda5cb46205b4
rnercolini <106276254+rnercolini@users.noreply.github.com>
2022-10-10 16:01:35 -0300

    Add files via upload

---
+++ Desafio32.py
@@ -0,0 +1,10 @@
+# Verifica se um ano é bissexto.
+from datetime import date
+ano = int(input('Digite um ano para ver se é bissexto. Para analisar o ano atual digite 0: '))
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano {} é bissexto.'.format(ano))
+else:
+    print('O ano {} não é bissexto.'.format(ano))
+    
